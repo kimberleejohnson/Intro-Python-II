8994f5d86cfeadfabb912991ad0f5a52589a9371
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-09-04 13:22:13 -0700

    Added empty init file to solve the errors I was getting from the linter

--- src/adv.py
+++ src/adv.py
@@ -1,39 +1,39 @@
 # Importing sys so it will be easy to exit out 
 import sys 
 # from player import Player 
-# from room import Room
+from room import Room
 
 # Declare all the rooms
-# room = {
-#     'outside':  Room("Outside Cave Entrance",
-#                      "North of you, the cave mount beckons"),
+room = {
+    'outside':  Room("Outside Cave Entrance",
+                     "North of you, the cave mount beckons"),
 
-#     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-# passages run north and east."""),
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+ passages run north and east."""),
 
-#     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-# into the darkness. Ahead to the north, a light flickers in
-# the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+ into the darkness. Ahead to the north, a light flickers in
+ the distance, but there is no way across the chasm."""),
 
-#     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-# to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+ to north. The smell of gold permeates the air."""),
 
-#     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-# chamber! Sadly, it has already been completely emptied by
-# earlier adventurers. The only exit is to the south."""),
-# }
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+ chamber! Sadly, it has already been completely emptied by
+ earlier adventurers. The only exit is to the south."""),
+}
 
 
 # Link rooms together
 
-# room['outside'].n_to = room['foyer']
-# room['foyer'].s_to = room['outside']
-# room['foyer'].n_to = room['overlook']
-# room['foyer'].e_to = room['narrow']
-# room['overlook'].s_to = room['foyer']
-# room['narrow'].w_to = room['foyer']
-# room['narrow'].n_to = room['treasure']
-# room['treasure'].s_to = room['narrow']
+room['outside'].n_to = room['foyer']
+room['foyer'].s_to = room['outside']
+room['foyer'].n_to = room['overlook']
+room['foyer'].e_to = room['narrow']
+room['overlook'].s_to = room['foyer']
+room['narrow'].w_to = room['foyer']
+room['narrow'].n_to = room['treasure']
+room['treasure'].s_to = room['narrow']
 
 #
 # Main
@@ -49,8 +49,7 @@ import sys
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+
 
 # Setting up Boolean for status of game
 active_session = True
@@ -60,8 +59,7 @@ while active_session:
 
     user_direction = input("\nEnter a cardinal direction your player should move to \n n for North \n e for East \n s for South \n w for West \n q to Quit \n Your input: ").lower()
 
+    # If the user enters "q", quit the game.
     if user_direction == 'q': 
         print("Thanks for playing. Goodbye!")
         sys.exit(1)
-    else: 
-        print("You didn't press q.")
--- src/room.py
+++ src/room.py
@@ -1,8 +1,15 @@
-# Implement a class to hold room information. This should have name and
-# description attributes.
+# Class to hold room information.
+# Includes name, description, 
+# And a directional attribute set to none because it's specified in adv.py
+# This is not assigned as a parameter here in this class because it's adv.py 
 
-# class Room: 
-#     def __init__(self, name, location, description): 
-#         self.name = name
-#         self.location = location 
-#         self.description = description 
+
+class Room:
+     def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None 
+        self.w_to = None 
+        
